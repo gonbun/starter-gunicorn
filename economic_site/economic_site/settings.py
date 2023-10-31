834131c84715ad56fe9c03731e46da3665549633
Gonbun <gonbun@promised-land.info>
2023-11-01 02:29:36 +0900

    fix django

--- economic_site/economic_site/settings.py
+++ economic_site/economic_site/settings.py
@@ -11,14 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import environ
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env()
-CYCLIC_URL = env["CYCLIC_URL"]
+CYCLIC_URL = os.environ.get("CYCLIC_URL")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
